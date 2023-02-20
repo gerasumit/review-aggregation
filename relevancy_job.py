2bbcdd2d3a53b05a377869b8406fd05bf04e8f91
Sumit Gera <gerasumit.dce@gmail.com>
2023-02-21 00:50:21 +0530

    Fixed query for shopee

--- relevancy_job.py
+++ relevancy_job.py
@@ -3,9 +3,16 @@ from details.scraper import ProductDetailScraper
 from google.query_builder import QueryBuilder, QueryConfig
 from google.search_api import OpenSearch
 import time
+from enum import Enum
+import sys, time
 
+class URLRelevancy(Enum):
+    WHITELISTED = 1
+    BLACKLISTED = 2
+    REMOVED = 3
+    UKNOWN = 4
 
-def getUrlRelevancy(url: str) -> str:
+def getUrlRelevancy(url: str) -> URLRelevancy:
     whitelisted_url_scheme = ["lazada.sg/products"]
     blacklisted_url_scheme = ["shopee.sg/search", "lazada.sg/tag", "shopee.sg/mall", "shopee.sg/list", "shopee.sg/collections", "amazon.sg/gp/bestsellers", "amazon.sg/stores"]
     URL_NOT_FOUND = -1
@@ -13,13 +20,13 @@ def getUrlRelevancy(url: str) -> str:
 
     for w_url_scheme in whitelisted_url_scheme:
         if url.find(w_url_scheme) != URL_NOT_FOUND:
-            return "Whitelisted"
+            return URLRelevancy.WHITELISTED
 
     for b_url_scheme in blacklisted_url_scheme:
         if url.find(b_url_scheme) != URL_NOT_FOUND:
-            return "Blacklisted"
+            return URLRelevancy.BLACKLISTED
 
-    return "Unknown"
+    return URLRelevancy.UKNOWN
 
 urls_file_ref = 'test_urls.csv'
 result_file_ref = 'relevancy_check.csv'
@@ -27,11 +34,15 @@ result_file_ref = 'relevancy_check.csv'
 with open(urls_file_ref) as urls_csv_file, open(result_file_ref, 'w') as result_file:
     urls_reader = csv.reader(urls_csv_file)
     result_writer = csv.writer(result_file)
-    result_writer.writerow(["Product Url", "Google Link", "Google Link Relevancy", "Time Taken"])
+    result_writer.writerow(["Product Url", "Google Link", "Query", "Google Link Relevancy", "Time Taken"])
+    
     for row in urls_reader:
+        # Processing item ...
+        print(f"Processing item {urls_reader.line_num}...", end='\r')
+
         start_time = time.time()
         productUrl = row[0]
-        productDetail = ProductDetailScraper(start_url=productUrl).request()
+        productDetail, isRemoved = ProductDetailScraper(start_url=productUrl).request()
         query = QueryBuilder().build(config=QueryConfig(productDetail=productDetail))
         search_results = OpenSearch().get_search_results(q=query)
         items = search_results['items'] if 'items' in search_results else []
@@ -40,4 +51,6 @@ with open(urls_file_ref) as urls_csv_file, open(result_file_ref, 'w') as result_
 
         for googleLink in googleLinks:
             url_relevancy = getUrlRelevancy(googleLink)
-            result_writer.writerow([productUrl, googleLink, url_relevancy, end_time-start_time])
+            if isRemoved:
+                url_relevancy = URLRelevancy.REMOVED
+            result_writer.writerow([productUrl, googleLink, query, url_relevancy, end_time-start_time])
