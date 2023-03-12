03fa11643d1c41ace385ae569372d9bb6bf4428b
Sumit Gera <gerasumit.dce@gmail.com>
2023-03-12 14:03:22 +0530

    Removed key

--- google/search_api.py
+++ google/search_api.py
@@ -3,8 +3,6 @@ import requests
 class OpenSearch:
     def get_search_results(self, q: str):
         params = {}
-        params["key"] = "AIzaSyCxDcYFFKXRqBbdb9j8UHZDHDPwff2TPBQ"
-        params["cx"] = "03cc508b073224774"
         params["q"] = q
         
         response = requests.get("https://www.googleapis.com/customsearch/v1", params=params)
